40b8c0798da92b5ed1b7b77bbf8c9b1eb3e24d69
paul.42 <paul.42@gmail.com>
2017-06-28 07:25:40 -0700

    Add very many comments

--- miniPhotoBooth2.py
+++ miniPhotoBooth2.py
@@ -11,14 +11,19 @@ import sys
 import cups
 import syslog
 
+#Prerequisites:
+# find out what version of python your raspberry pi uses by typing 'python --version' on the command line
+# install PILLOW (the replacement for PIL, aka Python Image Library) by googling 'install pillow python ###' where ### is your python version
+
+#general setup things here
 syslog.syslog('Starting Photobooth')
 os.system("amixer cset numid=3 1")
-xPhotoResolution = 800
+xPhotoResolution = 800 #screen resolution, not photo resolution
 yPhotoResolution = 480
 
 try:
     camera = picamera.PiCamera()
-    camera.resolution = (1120,750)
+    camera.resolution = (1120, 750)
     camera.vflip = True
     camera.hflip = False
     syslog.syslog('start preview')
@@ -32,13 +37,13 @@ except Exception:
 
 try:
     pygame.init()
-    window = pygame.display.set_mode((xPhotoResolution, yPhotoResolution))
-    pygame.display.set_caption('Chase and Abby!')
-    background = pygame.Surface(window.get_size())
-    background.fill((0, 0, 0))
+    window = pygame.display.set_mode((xPhotoResolution, yPhotoResolution)) ##This is the 'base' layer that will get all the updates and redraws - this is a global variable.
+    pygame.display.set_caption('Chase and Abby!') #does not actually do anything unless you have a titlebar...
+    background = pygame.Surface(window.get_size()) #when you want to make a change, make a new 'surface', we called our surface variable 'background' for this round.
+    background.fill((0, 0, 0)) # do something to the surface, like filling it. note the double parentheses, I think they are important
     font = pygame.font.Font(None, 700)
     messageFont = pygame.font.Font(None, 200)
-    countFont = pygame.font.Font(None, 80)
+    countFont = pygame.font.Font(None, 80) #make a font object
 except Exception:
     syslog.syslog('Error in pygame setup')
     sys.exit()
@@ -61,18 +66,18 @@ def startCountdown(photoCount):
                 message = ':)'
             else:
                 message = countDown - 1
-            background = pygame.Surface(window.get_size())
-            text = font.render(str(message), 1, (255, 255, 255))
+            background = pygame.Surface(window.get_size()) #fun example 2: make a surface and call it something memorable
+            text = font.render(str(message), 1, (255, 255, 255)) #rendering font (see line #44 where I initalized this variable with the 'string' of my message variable, 1, and the color white, see https://www.pygame.org/docs/ref/font.html#pygame.font.Font.render
             photosRemain = '%s / 4' % (photoCount)
             photoCountbox = countFont.render(str(photosRemain), 1, (0, 255, 0))
-            msgRectObj = photoCountbox.get_rect()
-            msgRectObj.topleft = (10, 20)
-            xPos = (xPhotoResolution // 2) - (text.get_width() // 2)
-            yPos = (yPhotoResolution // 2) - (text.get_height() // 2)
-            background.blit(text, (xPos, yPos))
-            background.blit(photoCountbox, msgRectObj)
-            window.blit(background, (0, 0))
-            pygame.display.flip()
+            msgRectObj = photoCountbox.get_rect() #get size of our rendering
+            msgRectObj.topleft = (10, 20) #set the topleft corner to start at x=10, y=20 pixels... forgetting why I put this here, actually
+            xPos = (xPhotoResolution // 2) - (text.get_width() // 2) #get the center start for x
+            yPos = (yPhotoResolution // 2) - (text.get_height() // 2)#get the center start for y
+            background.blit(text, (xPos, yPos)) #BLIT is Block level image transfer. use .blit to put one image ontop of another, and blit your final image onto window and .flip() to update. see https://www.pygame.org/docs/ref/surface.html#pygame.Surface.blit
+            background.blit(photoCountbox, msgRectObj) #here we are putting the msgRectObject onto the source image of 'photoCountBox', from the docs: 'Dest can either be pair of coordinates representing the upper left corner of the source. A Rect can also be passed as the destination and the topleft corner of the rectangle will be used as the position for the blit. The size of the destination rectangle does not effect the blit.' I guess that explains why I did the .topleft assignment earlier
+            window.blit(background, (0, 0)) #put our background image onto the window surface
+            pygame.display.flip() #tell pygame to update
             if countDown == 1:
                 return
             else:
@@ -99,12 +104,12 @@ def compileMessage():
 def blackScreen():
     try:
         syslog.syslog('blanking out screen')
-        img = pygame.image.load('touchtesttrans.tif')
-        background2 = pygame.Surface(window.get_size())
-        background2.fill((0, 0, 0))
-        background2.blit(img, (0, 0))
-        window.blit(background2, (0, 0))
-        pygame.display.flip()
+        img = pygame.image.load('touchtesttrans.tif') #fun example 3: blitting an image onto the window - make certain the image is the right size or pygame complains and crashes out.
+        background2 = pygame.Surface(window.get_size()) #get a new surface the size of the window
+        background2.fill((0, 0, 0)) #optional: background fille
+        background2.blit(img, (0, 0)) #place image starting at 0,0)
+        window.blit(background2, (0, 0)) #place background onto window
+        pygame.display.flip() #tell pygame to update.
         syslog.syslog('flipped')
     except Exception:
         syslog.syslog('blackScreen errored')
@@ -148,21 +153,21 @@ def tile_images_wrapper():
 
 def manually_arrange():
     try:
-        syslog.syslog('inside manually arrange')
-        isize = (1200, 3600)
-        first = (40, 40, 1160, 790)
+        syslog.syslog('inside manually arrange') #fun example 4: arranging 4x smaller photos into a larger photo strip!
+        isize = (1200, 3600) #this is the final size in pixels of the strip!
+        first = (40, 40, 1160, 790) #this is the rectangle that is the first photo
         second = (40, 830, 1160, 1580)
         third = (40, 1620, 1160, 2370)
         fourth = (40, 2409, 1160, 3159)
-        label = (40, 3199, 1160, 3560)
+        label = (40, 3199, 1160, 3560) #this is the label
         white = (255, 255, 255)
-        inew = Image.new('RGB', isize, white)
+        inew = Image.new('RGB', isize, white) #using Python Image Library fork (PILLOW)
         syslog.syslog('open img4')
         img4time = time.time()
         img = Image.open('img4.jpg')
-        inew.paste(img, first)
+        inew.paste(img, first) #if the image you open does not fit exactly into the same pixel size, python complains and crashes.
         syslog.syslog('pasted img4 in' + str('%.2fs' % (time.time() - img4time)))
-        # print('pasted')
+        # print('pasted') #ignore timing and diagnostic information.
         img3time = time.time()
         img = Image.open('img3.jpg')
         inew.paste(img, second)
@@ -190,28 +195,28 @@ def manually_arrange():
         syslog.syslog('Pasted Label' + str('%.2fs' % (time.time() - imgLabeltime)))
         start = time.time()
         fileName = './archive/%s.tiff' % (timestamp())
-        inew.save(fileName)
+        inew.save(fileName) #save the file
         syslog.syslog('Save took ' + str('%.2fs' % (time.time() - start)))
         start = time.time()
-        # job_id = conn.printFile('EPSON_PictureMate_PM_225', fileName, 'test',{})
+        # job_id = conn.printFile('EPSON_PictureMate_PM_225', fileName, 'test',{}) #CUPS is common unix print server. this sends the photo there and gets a jobid in return. if you're not using CUPS, comment this out, because I try to log the ID of it in a silly way.
         # syslog.syslog('job_id: '+ str(job_id))
         syslog.syslog('Send took ' + str('%.2fs' % (time.time() - start)))
     except:
         syslog.syslog('error in manual arrange')
         pass
 
-
+#Actual important looping stuff starts here.
 syslog.syslog('starting loop?')
-blackScreen()
+blackScreen() #set up the base 'touch here to start' screen
 # MAIN
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-        elif event.type == pygame.MOUSEBUTTONUP:
-            buttonPressed()
-            blackScreen()
-        elif event.type == KEYDOWN:
+        elif event.type == pygame.MOUSEBUTTONUP: #for the touchscreen we use, it registers a touch as a mouse click, we want to do something when they take their finger off the screen
+            buttonPressed() #do the things
+            blackScreen() #reset the screen to blank background with 'touch here to start' message
+        elif event.type == KEYDOWN: #if there's a keyboard attached, any keypress will exit. might want to configure this differently?
             pygame.quit()
             sys.exit()
